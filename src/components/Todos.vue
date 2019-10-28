09914920322fcb931d8520b3b2a197b458f22768
Bassem <bassem.magdyy94@gmail.com>
2019-10-28 03:32:45 +0200

    adding logic with some light styling

---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="Todo">
+        <!-- <h1>ay 7aga</h1> -->
+        <div v-for="todo in todos" v-bind:key="todo.id">
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"></TodoItem>
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: 'Todos',
+    components: {
+        TodoItem
+    },
+    props: ['todos']
+};
+</script>
+<style scoped>
+</style> 
